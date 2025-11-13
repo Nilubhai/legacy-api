afdf240685e542c9a27a4f15385af9be2e37bba8
Nilubhai <103625426+Nilubhai@users.noreply.github.com>
2025-11-13 07:25:15 -0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,20 @@
+export default function handler(req, res) {
+  const users = [
+    { email: "john@example.com", password: "Password123!" },
+    { email: "jane@example.com", password: "Test@123" },
+  ];
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Only POST supported" });
+  }
+
+  const { email, password } = req.body || {};
+
+  const user = users.find(u => u.email === email && u.password === password);
+
+  if (user) {
+    res.status(200).json({ email: user.email });
+  } else {
+    res.status(401).json({ message: "Invalid credentials" });
+  }
+}
